a537eee40475221f4ecadb20319eba068a490c7d
derek.t <jay15003939@gmail.com>
2022-06-01 16:09:09 +0800

    feat: vue3 init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
